510fb7f5e06de3b52efac6e0bc00c1e13e43151f
Nguyễn Anh Hào <anhhao012004@gmail.com>
2025-01-05 12:34:52 +0700

    feat: view list of orders sorted by order creation time

--- model/ordersModel.js
+++ model/ordersModel.js
@@ -11,9 +11,32 @@ module.exports = {
     async filterByCondition(condition, limit, offset, orderBy = "orderid") {
         let result;
         if (condition) {
-            result = await db.query(`SELECT o.*, u.username FROM ${tableName} o JOIN users u ON o.userid = u.userid WHERE ${condition} ORDER BY ${orderBy} DESC LIMIT ${limit} OFFSET ${offset}`);
+            result = await db.query(`SELECT o.*, u.username, 
+            CASE 
+                WHEN o.status = 0 THEN 'Pending'
+                WHEN o.status = 1 THEN 'Processing'
+                WHEN o.status = 2 THEN 'Completed'
+                WHEN o.status = 3 THEN 'Cancelled'
+            END as status
+            FROM ${tableName} o 
+            JOIN users u ON o.userid = u.userid 
+            WHERE ${condition} 
+            ORDER BY ${orderBy} DESC 
+            LIMIT ${limit} 
+            OFFSET ${offset}`);
         } else {
-            result = await db.query(`SELECT o.*, u.username FROM ${tableName} o JOIN users u ON o.userid = u.userid ORDER BY ${orderBy} DESC LIMIT ${limit} OFFSET ${offset}`);
+            result = await db.query(`SELECT o.*, u.username, 
+            CASE 
+                WHEN o.status = 0 THEN 'Pending'
+                WHEN o.status = 1 THEN 'Processing'
+                WHEN o.status = 2 THEN 'Completed'
+                WHEN o.status = 3 THEN 'Cancelled'
+            END as status
+            FROM ${tableName} o 
+            JOIN users u ON o.userid = u.userid 
+            ORDER BY ${orderBy} DESC 
+            LIMIT ${limit} 
+            OFFSET ${offset}`);
         }
         return result.rows;
     },
